9bb5d9beb02fc53d384fb42b77d072ff93d0cadb
Leo6016 <54263518+Leo6016@users.noreply.github.com>
2022-11-06 17:31:31 -0500

    Update thisday.py

--- src/thisday/thisday.py
+++ src/thisday/thisday.py
@@ -1,15 +1,7 @@
-from datetime import date
-data=[]
-
 def ProcessInput(inputString):
-    today = date.today()
-    if(inputString=="film"):
-        event=data[today][0]
-    elif(inputString=="history"):
-        event=data[today][1]
-    elif(inputString=="music"):
-        event=data[today][2]
-    elif(inputString=="sport"):
-        event=data[today][3]
-    ret=today+event
-    return ret
+    validInput=['film-tv','history','sport','music']
+    if inputString in validInput:
+        return inputString
+    else:
+        print("Incorrect Input, please try again.")
+    
