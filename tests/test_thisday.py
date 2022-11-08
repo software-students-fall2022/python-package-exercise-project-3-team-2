8f5c78f3ad1ffb37b4fdeeb06c920fd235d0c141
agarwalanvi01 <aa7140@stern.nyu.edu>
2022-11-07 21:58:02 -0500

    Adding new unit tests and editing connect and get_events functions

--- tests/test_thisday.py
+++ tests/test_thisday.py
@@ -29,18 +29,34 @@ class Tests:
         assert thisday.process_input("music")=="music","music doesn't run correctly"
         assert thisday.process_input("foo")==0,"other cases doesn't run correctly"
     
-    def test_connect(self):
+    def test_connect_valid(self):
+        assert thisday.connect("history")!=0, "is not allowing valid input"
+        assert thisday.connect("film-tv")!=0, "is not allowing valid input"
+        assert thisday.connect("music")!=0, "is not allowing valid input"
+        assert thisday.connect("sport")!=0, "is not allowing valid input"
+
+    def test_connect_invalid(self):
+        assert thisday.connect("random-topic")==0, "is allowing invalid input"
+
+    def test_connect_returntype(self):
         assert isinstance(thisday.connect("history"), BeautifulSoup), "did not return BeautifulSoup object"
         assert isinstance(thisday.connect("film-tv"), BeautifulSoup), "did not return BeautifulSoup object"
         assert isinstance(thisday.connect("sport"), BeautifulSoup), "did not return BeautifulSoup object"
         assert isinstance(thisday.connect("music"), BeautifulSoup), "did not return BeautifulSoup object"
 
+    def test_get_events_valid(self):
+        assert thisday.get_events(BeautifulSoup)!=0, "is not taking valid input"
 
-    def test_get_events(self):
+    def test_get_events_invalid(self):
+        assert thisday.get_events("random-input")==0, "is taking invalid input"
+
+    def test_get_events_returntype(self):
         assert isinstance(thisday.get_events(thisday.connect("history")), list), "did not return list of events"
         assert isinstance(thisday.get_events(thisday.connect("film-tv")), list), "did not return list of events"
         assert isinstance(thisday.get_events(thisday.connect("sport")), list), "did not return list of events"
         assert isinstance(thisday.get_events(thisday.connect("music")), list), "did not return list of events"
+
+    def test_get_events_listcontent(self):   
         assert len(thisday.get_events(thisday.connect("history")))!=0, "list of events is empty"
         assert len(thisday.get_events(thisday.connect("film-tv")))!=0, "list of events is empty"
         assert len(thisday.get_events(thisday.connect("sport")))!=0, "list of events is empty"
