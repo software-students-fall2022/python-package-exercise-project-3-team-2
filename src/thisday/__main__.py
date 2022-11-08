86ed4146114913637fa8a325df7bdb5b2abff79c
danilo-montes <dem9855@nyu.edu>
2022-11-08 00:20:52 -0500

    uncommented package functionality in main function

--- src/thisday/__main__.py
+++ src/thisday/__main__.py
@@ -2,13 +2,10 @@ import thisday.thisday as thisday
 import sys
 
 def main():
-    print('called from command line!') # placeholder
-    '''
     option = thisday.validate(sys.argv)
     dom = thisday.connect(option)
     li = thisday.get_events(dom)
     print(thisday.show(li))
-    '''
 
 if __name__ == '__main__':
     main()
